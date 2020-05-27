14394f54c6bdc6fa6f996532de6d3c3062cf3ae9
DamienOuz <59994661+DamienOuz@users.noreply.github.com>
2020-05-27 17:07:29 +0200

    Add files via upload

--- IlePerdueTEST/src/CVue.java
+++ IlePerdueTEST/src/CVue.java
@@ -9,7 +9,7 @@ import javax.swing.*;
  *  - Une zone d'affichage où on voit l'ensemble des Zones.
  *  - Une zone de commande avec un bouton pour passer à la génération suivante.
  */
-class CVue {
+class CVue{
     /**
      * JFrame est une classe fournie pas Swing. Elle représente la fenêtre
      * de l'application graphique.
@@ -27,7 +27,9 @@ class CVue {
 	/** Définition de la fenêtre principale. */
 	frame = new JFrame();
 	JLabel tourJoueur = new JLabel("<html><h1>C'est le tour du joueur en rouge</h1><html>", JLabel.CENTER);
+	JLabel actions = new JLabel("              Il reste " + (3 - modele.nbActions) + " actions", JLabel.LEFT);
 	frame.add(tourJoueur);
+	frame.add(actions);
 	frame.setTitle("Jeu de la vie de Conway");
 	
 	/**
@@ -48,7 +50,7 @@ class CVue {
 	frame.setLayout(new FlowLayout());
 
 	/** Définition des deux vues et ajout à la fenêtre. */
-	grille = new VueGrille(modele);
+	grille = new VueGrille(modele, actions);
 	frame.add(grille);
 	commandes = new VueCommandes(modele);
 	frame.add(commandes);
@@ -73,4 +75,5 @@ class CVue {
 	frame.setSize(600, 400);
 	frame.setVisible(true);
     }
+    
 }
